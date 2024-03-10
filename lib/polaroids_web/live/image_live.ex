b9b3612a6ac0f6425f638dcb65acbcb2ac3cca7c
hermlon <hermlon@yuustan.space>
2024-03-10 21:49:56 +0100

    Disable delete button for now

--- lib/polaroids_web/live/image_live.ex
+++ lib/polaroids_web/live/image_live.ex
@@ -8,15 +8,15 @@ defmodule PolaroidsWeb.ImageLive do
     socket = assign(socket, :image, gallery_image)
     edit_url = gallery_image.meta && Application.fetch_env!(:polaroids, :edit_url)
     socket = assign(socket, :edit_url, edit_url && (edit_url <> gallery_image.meta))
-
+    socket = assign(socket, :is_admin, false)
     {:ok, socket}
   end
 
-  def handle_event("remove", %{"id" => key}, socket) do
-    PubSub.broadcast!(Polaroids.PubSub, "gallery", %{event: "delete", key: key})
-    Gallery.delete_image(key)
-    {:noreply, push_navigate(socket, to: ~p"/g/#{Gallery.Image.gallery(key)}", replace: true)}
-  end
+  #def handle_event("remove", %{"id" => key}, socket) do
+  #  PubSub.broadcast!(Polaroids.PubSub, "gallery", %{event: "delete", key: key})
+  #  Gallery.delete_image(key)
+  #  {:noreply, push_navigate(socket, to: ~p"/g/#{Gallery.Image.gallery(key)}", replace: true)}
+  #end
 
   def handle_event("share", %{"id" => key}, socket) do
     {:noreply, push_event(socket, "share", %{image: Gallery.static_url(key)})}
